efa42e752c29a44fb03ab8224a4c4ac5d37c7b52
Shigenori Taya <shigenori.taya@gmail.com>
2014-12-27 23:59:56 +0900

    modifying BM (chap 16)

--- 16_BM/bm.hs
+++ 16_BM/bm.hs
@@ -1,31 +1,42 @@
 
 matches :: Eq a => [a] -> [a] -> Int
-matches ws = map length . filter (endswith ws) . inits
-
+--matches ws = map length . filter (endswith ws) . inits
 --matches "abcab" "ababcabcab" = [7, 10]
 
 -- scan lemma
-map (foldl op e) . inits = scanl op e
-
-map f . filter p = map fst . filter snd . map (fork (f, p))
-
-matches ws
-   = map fst . filter snd . map (fork (length, endswith ws)) . inits
-
-endswith ws = foldl op e
+--map (foldl op e) . inits = scanl op e
+--map f . filter p = map fst . filter snd . map (fork (f, p))
+--matches ws
+--   = map fst . filter snd . map (fork (length, endswith ws)) . inits
+--endswith ws = foldl op e
 
 --the tupling law for foldl
-fork (foldl op1 e1, foldl op2 e2) = foldl op (e1, e2)
+--fork (foldl op1 e1, foldl op2 e2) = foldl op (e1, e2)
+--fork (length, endswith ws) = foldl step (0, e)
+--step (n, x) y              = (n + 1, op x y)
+--matches ws = map fst . filter snd . scanl step (0, e)
+--endswith ws = p . foldl op e
+--map f . filter (p . g)
+--  = map fst . filter (p . snd) . map (fork (f, g))
+--matches ws = map fst . filter (p . snd) . scanl step (0, e)
 
-fork (length, endswith ws) = foldl step (0, e)
-step (n, x) y              = (n + 1, op x y)
+matches ws                = test m . scanl step (0, [ ])
+  where
+    test j [ ]            = [ ]
+    test j ((n, sx): nxs) | i == m     = n: test k (drop (k - 1) nxs)
+                          | m - k <= i = test k (drop (k - 1) nxs)
+                          | otherwise  = test m (drop (k - 1) nxs)
+                            where
+                              i' = llcp sw (take j sx)
+                              i  = if i' == j then m else i'
+                              k  = shift sw i
+    (sw, m)               = (reverse ws, length ws)
+    shift sw i
+        = head [k | k <- [1..m], llcp sw (drop k sw) == min i (m - k)]
 
-matches ws = map fst . filter snd . scanl step (0, e)
+step (n, sx) x = (n + 1, x: sx)
 
-endswith ws = p . foldl op e
-
-map f . filter (p . g)
-  = map fst . filter (p . snd) . map (fork (f, g))
-
-matches ws = map fst . filter (p . snd) . scanl step (0, e)
+llcp xs  [ ]        = 0
+llcp [ ] ys         = 0
+llcp (x: xs) (y:ys) = if x == y then 1 + llcp xs ys else 0
 
