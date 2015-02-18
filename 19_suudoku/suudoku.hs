37b1c2128fa2981f171b0d587a8bc1312e287da3
Shigenori Taya <shigenori.taya@gmail.com>
2015-02-18 21:42:14 +0900

    fix bugs in suudoku

--- 19_suudoku/suudoku.hs
+++ 19_suudoku/suudoku.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 -- 19.2. 仕様
 type Matrix a = [Row a]
 type Row a = [a]
@@ -5,22 +7,25 @@ type Row a = [a]
 type Grid = Matrix Digit
 type Digit = Char
 
-digits = [1..9]
-blank = (== 0)
+--digits = [1..9]
+--blank = (== 0)
+--digits = "123456789"
+digits = ['1'..'9']
+blank = (== '0')
 
 --solve version 1
 solve1 = filter valid . expand . choices
 
-choices :: Grid -> Matrix Choices
-expand  :: Matrix Choices -> [Grid]
-valid   :: Grid -> Bool
+--choices :: Grid -> Matrix Choices
+--expand  :: Matrix Choices -> [Grid]
+--valid   :: Grid -> Bool
 
 --choices
 type Choices = [Digit]
 
-choices :: Grid -> Matrix Choices
-choices = map (map choices)
-choices d = if blank d then digits else [d]
+choices  :: Grid -> Matrix Choices
+choices  = map (map choice)
+choice d = if blank d then digits else [d]
 
 --expand
 expand :: Matrix Choices -> [Grid]
@@ -28,7 +33,7 @@ expand = cp . map cp
 
 cp           :: [[a]] -> [[a]]
 cp []        = [[]]
-cp (xs: xss) = [x:ys | x <- xs, ys <- cp xs]
+cp (xs: xss) = [x:ys | x <- xs, ys <- cp xss]
 
 --valid
 valid :: Grid -> Bool
@@ -48,11 +53,11 @@ cols [xs] = [[x] | x <- xs]
 cols (xs: xss) = zipWith (:) xs (cols xss)
 
 boxs :: Matrix a -> Matrix a
-boxs = map ungroup . ungroup . map cols . group . map group
+boxs = map ungroup . ungroup . map cols . _group . map _group
 
-group    :: [a] -> [[a]]
-group [] = []
-group xs = take 3 xs: group (drop 3 xs)
+_group    :: [a] -> [[a]]
+_group [] = []
+_group xs = take 3 xs: _group (drop 3 xs)
 
 ungroup :: [[a]] -> [a]
 ungroup = concat
@@ -74,6 +79,7 @@ pruneRow row = map (remove fixed) row
 
 remove xs ds = if single ds then ds else ds \\ xs
 --filter nodups . cp = filter nodups . cp . pruneRow --(19.4)
+single xs = (length xs == 1)
 
 --if f.f = id then
 --filter (p . f) = map f . filter p . map f --(19.5)
@@ -84,38 +90,38 @@ remove xs ds = if single ds then ds else ds \\ xs
 --    = filter (all nodups . boxs) . expand . pruneBy boxs
 pruneBy f = f . map pruneRow . f
 
-filter valid . expand = filter valid . expand . prune
+--filter valid . expand = filter valid . expand . prune
 prune = pruneBy boxs . pruneBy cols . pruneBy rows
 
 -- solve version 2
 solve2 = filter valid . expand . prune . choices
 
 -- 19.4. 単一マス拡張
-expand = concat . map expand . expand1 --(19.7)
+--expand = concat . map expand . expand1 --(19.7)
 
 expand1      :: Matrix Choices -> [Matrix Choices]
 expand1 rows = [rows1
                 ++ [row1 ++ [c]: row2]
-				++ rows2 | c <- cs]
-  where (rows1, row: rows2) = break (any smallest) rows
-        (row1,  cs:  row2)  = break smallest row
-		smallest cs         = length cs == n
-		n                   = minimum (counts rows)
+                ++ rows2 | c <- cs]
+  where (rows1, row: rows2) = _break (any smallest) rows
+        (row1,  cs:  row2)  = _break smallest row
+        smallest cs         = length cs == n
+        n                   = minimum (counts rows)
 counts = filter (/= 1) . map length . concat
 
-break p xs = (takeWhile (not . p) xs, dropWhile (not . p) xs)
+_break p xs = (takeWhile (not . p) xs, dropWhile (not . p) xs)
 
 complete = all (all single)
 safe m = all ok (rows m) && all ok (cols m) && all ok (boxs m)
   where ok row = nodups [d | [d] <- row]
 
-search = filter valid . expand . prune
+--search = filter valid . expand . prune
 --search = concat . map search . expand1 . prune
 
 -- solve version 3
 solve3 = search . choices
 search m | not (safe m) = []
          | complete m'  = [map (map head) m']
-		 | otherwise    = concat (map search (expand1 m'))
-		   where m' = prune m
+         | otherwise    = concat (map search (expand1 m'))
+           where m' = prune m
 
