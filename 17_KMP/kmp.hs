77a1f55ad1aeea79bc8f4c635fc3ee5d79424af5
Shigenori Taya <shigenori.taya@gmail.com>
2015-02-28 04:31:17 +0900

    adding chap 17 KMP

--- 17_KMP/kmp.hs
+++ 17_KMP/kmp.hs
@@ -1,8 +1,19 @@
 
-endswith ws xs = ws `elem` tails xs
+--endswith ws xs = ws `elem` tails xs
 
-matches :: Eq a => [a] -> [a] -> Int
-matches ws = map fst . filter (p . snd) . scanl step (0, e)
-step (n, x) y = (n + 1, op x y)
+matches :: Eq a => [a] -> [a] -> [Int]
+--matches ws = map fst . filter (p . snd) . scanl step (0, e)
+--step (n, x) y = (n + 1, op x y)
 
+data Rep a = Null | Node a (Rep a) (Rep a)
 
+matches ws = map fst . filter (ok . snd) . scanl step (0, root)
+  where
+    ok (Node vs l r)        = null vs
+    step (n, t) x           = (n + 1, op t x)
+    op Null x               = root
+    op (Node []      l r) x = op l x
+    op (Node (v: vs) l r) x = if v == x then r else op l x
+    root                    = grep Null ws
+    grep l []               = Node []      l Null
+    grep l (v: vs)          = Node (v: vs) l (grep (op l v) vs)
