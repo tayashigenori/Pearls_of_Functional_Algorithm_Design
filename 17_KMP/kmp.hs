28e322b5f670a536c4e83f3acb06a492696f1af1
Shigenori Taya <shigenori.taya@gmail.com>
2015-03-01 02:34:32 +0900

    adding 17 (KMP) temporal code

--- 17_KMP/kmp.hs
+++ 17_KMP/kmp.hs
@@ -5,15 +5,16 @@ matches :: Eq a => [a] -> [a] -> [Int]
 --matches ws = map fst . filter (p . snd) . scanl step (0, e)
 --step (n, x) y = (n + 1, op x y)
 
-data Rep a = Null | Node a (Rep a) (Rep a)
+--split ws xs = head [(us, ws ↓ us) | us <- tails xs, us `isPrefixOf` ws]
+-- does not work!!
+--split ws []          = ([], ws)
+--split ws (xs ++ [x]) = op (split ws xs) x
 
-matches ws = map fst . filter (ok . snd) . scanl step (0, root)
-  where
-    ok (Node vs l r)        = null vs
-    step (n, t) x           = (n + 1, op t x)
-    op Null x               = root
-    op (Node []      l r) x = op l x
-    op (Node (v: vs) l r) x = if v == x then r else op l x
-    root                    = grep Null ws
-    grep l []               = Node []      l Null
-    grep l (v: vs)          = Node (v: vs) l (grep (op l v) vs)
+op (ws vs) x | [x] `isPrefixOf` vs = (us ++ [x], tail vs)
+             | null us             = ([], ws)
+             | otherwise           = op (split ws (tail us)) x
+               where ws = us ++ vs
+
+matches ws           = map fst . filter (null . snd . snd) . scanl step (0, ([], ws))
+step (n, (us, ws)) x = (n + 1, op (us, vs) x)
+
