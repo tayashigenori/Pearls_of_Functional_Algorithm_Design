0a31eec428193874197d5be062ec3a978243d1a1
Shigenori Taya <shigenori.taya@gmail.com>
2015-03-03 20:31:24 +0900

    fix a bug in kmp.hs (Chap 17)

--- 17_KMP/kmp.hs
+++ 17_KMP/kmp.hs
@@ -1,3 +1,4 @@
+import Data.List
 
 --endswith ws xs = ws `elem` tails xs
 
@@ -10,11 +11,18 @@ matches :: Eq a => [a] -> [a] -> [Int]
 --split ws []          = ([], ws)
 --split ws (xs ++ [x]) = op (split ws xs) x
 
-op (ws vs) x | [x] `isPrefixOf` vs = (us ++ [x], tail vs)
-             | null us             = ([], ws)
-             | otherwise           = op (split ws (tail us)) x
-               where ws = us ++ vs
+split ws []  = ([], ws)
+split ws [x] = op ([], ws) x
+split ws xs  = op (split ws (init xs)) (last xs)
+
+op (us, vs) x | [x] `isPrefixOf` vs = (us ++ [x], tail vs)
+              | null us             = ([], ws)
+              | otherwise           = op (split ws (tail us)) x
+                where ws = us ++ vs
 
 matches ws           = map fst . filter (null . snd . snd) . scanl step (0, ([], ws))
-step (n, (us, ws)) x = (n + 1, op (us, vs) x)
+step (n, (us, vs)) x = (n + 1, op (us, vs) x)
 
+
+-- *Main> (scanl step (0, ([], "abcab"))) "ababcabcab"
+-- [(0,("","abcab")),(1,("a","bcab")),(2,("ab","cab")),(3,("a","bcab")),(4,("ab","cab")),(5,("abc","ab")),(6,("abca","b")),(7,("abcab","")),(8,("abc","ab")),(9,("abca","b")),(10,("abcab",""))]
