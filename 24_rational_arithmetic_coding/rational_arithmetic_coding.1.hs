f93c9274e86886114546141b50b2ef58bf085e1b
Shigenori Taya <shigenori.taya@gmail.com>
2015-07-14 00:31:26 +0900

    adding chap 24 rational arithmetic coding

---
+++ 24_rational_arithmetic_coding/rational_arithmetic_coding.1.hs
@@ -0,0 +1,27 @@
+import Data.Ratio
+
+-- data
+
+type Fraction = Ratio Integer
+type Interval = (Fraction, Fraction)
+
+-- narrowing
+(|>)                 :: Interval -> Interval -> Interval
+(l1, r1) |> (l2, r2) = (l1 + (r1 - l1) * l2, l1 + (r1 - l1) * r2)
+
+(<|)        :: Fraction -> Interval -> Fraction
+f <| (l, r) = (f - l) / (r - l)
+
+-- model
+interval :: Model -> Symbol -> Interval
+symbol   :: Model -> Fraction -> Symbol
+
+adapt :: Model -> Symbol -> Model
+
+intervals           :: Model -> [Symbol] -> [Interval]
+intervals m []      = []
+intervals m (x: xs) = interval m x: intervals (adapt m x) xs
+
+-- encode
+encode   :: Model -> [Symbol] -> Fraction
+encode m = pick . foldl (|>) (0, 1) . intervals m
